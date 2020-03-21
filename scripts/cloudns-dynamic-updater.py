9248b13d34de289ee35bd67a87608878c7b7b4aa
SchroedingerHepcat <SchroedingerHepcat@gmail.com>
2020-03-20 19:51:33 -0600

    Add updater for cloudns

---
+++ scripts/cloudns-dynamic-updater.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+from pathlib import Path
+with open(Path.home() / ".cloudns") as f:
+    for line in f.readlines():
+        DYNAMIC_DNS_URL = line
+        if sys.version_info[0] < 3:
+            import urllib
+            page = urllib.urlopen(DYNAMIC_DNS_URL);
+            page.close();
+        else:
+            import urllib.request
+            page = urllib.request.urlopen(DYNAMIC_DNS_URL);
+            page.close();
