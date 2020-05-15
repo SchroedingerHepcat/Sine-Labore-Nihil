9b4337a52e62084180fb87af137d7d9fcf7f2173
Joshua Morain <SchroedingerHepcat@gmail.com>
2020-05-15 17:20:36 -0600

    Add monitor setup script for Alanna

--- scripts/monitorSetup.sh
+++ scripts/monitorSetup.sh
@@ -1,14 +1,1 @@
-#! /bin/bash
-xrandr --output DFP1 \
-       --preferred \
-       --pos 1080x0 \
-       --rotate normal \
-       --mode 3440x1440 \
-       --output DFP7 \
-       --left-of DFP1 \
-       --rotate left \
-       --mode 1920x1080 \
-       --output DFP6 \
-       --right-of DFP1 \
-       --rotate right \
-       --mode 1920x1080
+monitorSetupAlanna.sh
