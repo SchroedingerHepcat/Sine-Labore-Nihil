9992a20864a98c8fe232347474884c359d3230c5
Joshua Morain <SchroedingerHepcat@gmail.com>
2023-12-18 10:39:01 -0700

    Updated monitor names

--- scripts/monitorSetupAlanna.sh
+++ scripts/monitorSetupAlanna.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-xrandr --output DisplayPort-3 --pos 2560x1440 --auto --primary \
-       --output HDMI-A-1      --pos 0x1440    --auto           \
-       --output DisplayPort-4 --pos 5120x1440 --auto           \
-       --output DisplayPort-5 --pos 200x360   --auto           \
-       --output HDMI-1-1      --pos 2120x0    --auto           \
-       --output DP-1-1        --pos 5560x360  --auto
+xrandr --output DP-1 --pos 2560x1440 --auto --primary \
+       --output HDMI-1      --pos 0x1440    --auto           \
+       --output DP-2 --pos 5120x1440 --auto           \
+       --output DP-4 --pos 200x360   --auto           \
+       --output DP-6      --pos 2120x0    --auto           \
+       --output DP-7        --pos 5560x360  --auto
