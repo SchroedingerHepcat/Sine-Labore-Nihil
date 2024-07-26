9145212631d0345cc536610b63da08bdb0c9d073
Joshua Morain <SchroedingerHepcat@gmail.com>
2024-07-25 21:27:50 -0600

    Adapt to different displayport naming in xrandr

---
+++ scripts/monitorResetAlanna.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+~/.screenlayout/alanna-reset1.sh
+sleep 6
+~/.screenlayout/alanna-reset2.sh
+sleep 6
+~/.screenlayout/alanna-reset3.sh
+sleep 6
+~/.screenlayout/alanna-reset4.sh
+sleep 6
+~/.screenlayout/alanna-reset5.sh
+sleep 6
+~/scripts/monitorSetupAlanna.sh
--- scripts/monitorSetupAlanna.sh
+++ scripts/monitorSetupAlanna.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-xrandr --output DP-1 --primary --mode 2560x1440 --pos 2560x1440 --rotate normal \
-       --output DP-2 --mode 2560x1440 --pos 5120x1440 --rotate normal \
-       --output DP-3 --off \
-       --output HDMI-1 --mode 2560x1440 --pos 0x1440 --rotate normal \
-       --output DP-4 --mode 1920x1080 --pos 640x360 --rotate normal \
-       --output DP-5 --off \
-       --output DP-6 --mode 2560x1440 --pos 2560x0 --rotate normal \
-       --output DP-7 --off \
-       --output DP-8 --mode 1920x1080 --pos 5120x360 --rotate normal \
-       --output DP-9 --off
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x1440 --rotate normal \
+       --output DisplayPort-1 --mode 2560x1440 --pos 5120x1440 --rotate normal \
+       --output DisplayPort-2 --off \
+       --output HDMI-A-0 --mode 2560x1440 --pos 0x1440 --rotate normal \
+       --output DisplayPort-3 --mode 1920x1080 --pos 640x360 --rotate normal \
+       --output DisplayPort-4 --off \
+       --output DisplayPort-5 --mode 2560x1440 --pos 2560x0 --rotate normal \
+       --output DisplayPort-6 --off \
+       --output DisplayPort-7 --mode 1920x1080 --pos 5120x360 --rotate normal \
+       --output DisplayPort-8 --off
