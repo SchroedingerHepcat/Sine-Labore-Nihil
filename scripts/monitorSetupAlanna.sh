c41292f6b4e23494dfdbe6efb5bfe4a06770bef4
Joshua Morain <SchroedingerHepcat@gmail.com>
2024-07-25 21:18:31 -0600

    Monitor setup script for Alanna

--- scripts/monitorSetupAlanna.sh
+++ scripts/monitorSetupAlanna.sh
@@ -1,8 +1,11 @@
-#! /bin/bash
-
-xrandr --output DP-1    --pos 2560x1440 --auto --primary \
-       --output HDMI-1  --pos 0x1440    --auto           \
-       --output DP-2    --pos 5120x1440 --auto           \
-       --output DP-4    --pos 200x360   --mode 1920x1080 \
-       --output DP-6    --pos 2120x0    --auto           \
-       --output DP-7    --pos 5560x360  --mode 1920x1080
+#!/bin/sh
+xrandr --output DP-1 --primary --mode 2560x1440 --pos 2560x1440 --rotate normal \
+       --output DP-2 --mode 2560x1440 --pos 5120x1440 --rotate normal \
+       --output DP-3 --off \
+       --output HDMI-1 --mode 2560x1440 --pos 0x1440 --rotate normal \
+       --output DP-4 --mode 1920x1080 --pos 640x360 --rotate normal \
+       --output DP-5 --off \
+       --output DP-6 --mode 2560x1440 --pos 2560x0 --rotate normal \
+       --output DP-7 --off \
+       --output DP-8 --mode 1920x1080 --pos 5120x360 --rotate normal \
+       --output DP-9 --off
